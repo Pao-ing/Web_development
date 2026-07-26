ea5439e5f4429675cf208ff3aebc807668abec32
Pao-ing <paola.hernandez.porras1114@gmail.com>
2026-07-26 17:57:31 -0600

    Creating uncompress SugarBlossom folder

---
+++ SugarBlossomProject/SugarBlossomProject/Controllers/HomeSweetController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SugarBlossomProject.Controllers
+{
+    public class HomeSweetController : Controller
+    {
+        public IActionResult HomeSweet()
+        {
+            return View();
+        }
+        public IActionResult Services()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ SugarBlossomProject/SugarBlossomProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SugarBlossomProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
